0017ee4776ac6bbcf607d2e00f1edd65028e72c1
nubilaxl <nubila.levon@outlook.com>
2025-06-05 20:10:38 -0700

    typo

--- src/assets/API_KEY.ts
+++ src/assets/API_KEY.ts
@@ -1,4 +1,4 @@
 const MOCKAPI_URL = "https://68399d4f6561b8d882b10c6e.mockapi.io/meal"
 
-const JSONSERVER_URL = "http://localhost:3000/meals"
+//const JSONSERVER_URL = "http://localhost:3000/meals"
 export const API_URL = MOCKAPI_URL
